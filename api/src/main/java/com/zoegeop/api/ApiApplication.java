e63614ba8123a81bae6afdee41bb227af28ac6a3
ZoeGeop <zoegeop@gmail.com>
2020-09-06 16:11:50 -0500

    add db

--- api/src/main/java/com/zoegeop/api/ApiApplication.java
+++ api/src/main/java/com/zoegeop/api/ApiApplication.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ApiApplication {
-    private final GreetingRepository greetingRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
@@ -22,20 +20,28 @@ public class ApiApplication {
         };
     }
 
-	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+}
 
-	@GetMapping("/greetings")
-    public List<Greeting> greeting(){
-	    return greetingRepository.findAll();
+@RestController
+class HelloController{
+    private final GreetingRepository greetingRepository;
+
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
-    ApiApplication(GreetingRepository greetingRepository){
-	    this.greetingRepository = greetingRepository;
+    @GetMapping("/greetings")
+    public List<Greeting> greeting(){
+        return greetingRepository.findAll();
+    }
+
+
+    HelloController(GreetingRepository greetingRepository){
+        this.greetingRepository = greetingRepository;
     }
 }
+
 
 @Entity
 class Greeting{
