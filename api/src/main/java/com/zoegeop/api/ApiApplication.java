14a9845399cbf9100a88f89e1402b4c779cd391a
ZoeGeop <zoegeop@gmail.com>
2020-09-06 17:10:48 -0500

    add db

--- api/src/main/java/com/zoegeop/api/ApiApplication.java
+++ api/src/main/java/com/zoegeop/api/ApiApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
